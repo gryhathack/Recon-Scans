d055705fd1dd09c70e059f0ad68e92d5b3d897ea
nwiebelhaus <nwiebelhaus@DEN-400993L>
2017-02-27 12:52:31 -0700

    Commit snmprecon

---
+++ snmprecon/snmprecon.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+
+#Describes the usage if user input/usage is incorrect
+
+if len(sys.argv) != 2:
+    print "Usage: snmprecon.py <ip address>"
+    sys.exit(0)
+
+#Declare user input variable
+
+snmpdetect = 0
+ip_address = sys.argv[1]
+
+ONESIXONESCAN = "onesixtyone %s" % (ip_address)
+results = subprocess.check_output(ONESIXONESCAN, shell=True).strip()
+
+if results != "":
+    if "Windows" in results:
+        results = results.split("Software: ")[1]
+        snmpdetect = 1
+    elif "Linux" in results:
+        results = results.split("[public] ")[1]
+        snmpdetect = 1
+    if snmpdetect == 1:
+        print "[*] SNMP running on " + ip_address + "; OS Detect: " + results
+        SNMPWALK = "snmpwalk -c public -v1 %s 1 > /root/Desktop/results/%s_snmpwalk.txt" % (ip_address, ip_address)
+        results = subprocess.check_output(SNMPWALK, shell=True)
+
+NMAPSCAN = "nmap -vv -sV -sU -Pn -p 161,162 --script=snmp-netstat,snmp-processes %s" % (ip_address)
+results = subprocess.check_output(NMAPSCAN, shell=True)
+resultsfile = "root/Desktop/results/" + ip_address + "_snmprecon.txt"
+f = open(resultsfile, "w")
+f.write(results)
+f.close
